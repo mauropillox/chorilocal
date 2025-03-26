dc9dae9af671026ebe693062746c0482d0eb34d7
Mauro Guimarans <mauro.guimarans96@gmail.com>
2025-03-26 18:25:24 -0300

    SH de migraciones

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,11 @@ sudo docker-compose down
 
 echo "<dd42> Reconstruyendo e iniciando contenedores..."
 if sudo docker-compose up --build -d; then
+  echo "✅ Contenedores iniciados."
+
+  echo "<dd60> Aplicando migración de base de datos..."
+  sudo docker exec chorizaurio-backend-1 python3 backend/migraciones/migrar_db.py
+
   echo "✅ Deploy completo."
 else
   echo "❌ Error durante el build o inicio de contenedores."
