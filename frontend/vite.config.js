0565f7b628f734c14aacbeaf6dcaf9bc744f6206
Mauro Guimarans <mauro.guimarans96@gmail.com>
2025-04-04 17:03:04 -0300

    Fix Railway frontend deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,10 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000, // Este puerto se ignora en producción pero es útil localmente
+    port: 3000,
   },
   preview: {
-    port: process.env.PORT || 3000, // Railway lo sobrescribe con el puerto correcto
-    host: true
+    port: process.env.PORT || 3000,
+    host: true,
   }
 });
