a51675db7611aeea19c69c583ed769ee24f303f9
Mauro Guimarans <mauro.guimarans96@gmail.com>
2025-04-04 15:48:20 -0300

    fix: agregado script start para Railway

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // <- este es el cambio clave
-    port: 8080,
-    open: true,
+    host: '0.0.0.0',
+    port: 3000, // Este puerto se ignora en producción pero es útil localmente
   },
+  preview: {
+    port: process.env.PORT || 3000, // Railway lo sobrescribe con el puerto correcto
+    host: true
+  }
 });
