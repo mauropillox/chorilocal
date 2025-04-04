508a64a1352c36b7e23d2dd81da864a6954030a5
Mauro Guimarans <mauro.guimarans96@gmail.com>
2025-04-04 17:49:15 -0300

    backend

--- backend/create_admin.py
+++ backend/create_admin.py
@@ -1,20 +1,19 @@
-# backend_create_admin.py
-from db import crear_tablas, get_usuario as obtener_usuario_por_username, crear_usuario
+from db import crear_tablas, get_usuario as obtener_usuario_por_username, add_usuario
 from auth import obtener_password_hash
 
-def crear_admin_si_no_existe():
+def run():
     crear_tablas()
 
     if not obtener_usuario_por_username("admin"):
-        crear_usuario(
+        add_usuario(
             username="admin",
-            password=obtener_password_hash("admin"),
+            password_hash=obtener_password_hash("admin"),
             rol="admin",
-            activo=True
+            activo=1
         )
         print("✅ Usuario admin creado")
     else:
         print("ℹ️ Usuario admin ya existe")
 
 if __name__ == "__main__":
-    crear_admin_si_no_existe()
+    run()
